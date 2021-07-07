aded4cd03988537dd3920e7226b37ede3016e7ab
dao <phuong018@gmail.com>
2021-07-07 10:02:29 +0200

    Update: change tcp library to the implemented library

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,4 +1,4 @@
-#include "socket.hh"
+#include "tcp_sponge_socket.hh"
 #include "util.hh"
 
 #include <cstdlib>
@@ -12,10 +12,10 @@ void get_URL(const string &host, const string &path) {
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
-    TCPSocket tcp_socket;
+    CS144TCPSocket tcp_socket;
     tcp_socket.connect(Address(host, "http"));
     tcp_socket.write("GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n");
-    tcp_socket.shutdown(SHUT_WR);
+    //tcp_socket.shutdown(SHUT_WR);
 
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
@@ -23,7 +23,9 @@ void get_URL(const string &host, const string &path) {
     while (!tcp_socket.eof()) {
         cout << tcp_socket.read();
     }
-    tcp_socket.close();
+    //tcp_socket.close();
+
+    tcp_socket.wait_until_closed();
 
     cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
     cerr << "Warning: get_URL() has not been implemented yet.\n";
